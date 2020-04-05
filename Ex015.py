b78d1a65a5a86e3b45a3e61210735f4993c0149e
Felipe Pereira <felipewnp@gmail.com>
2020-04-05 20:21:42 -0300

    correção tipo variavel v_diaria

--- Ex015.py
+++ Ex015.py
@@ -12,7 +12,7 @@ def f_calcula_diaria_km(v_diaria,v_km):
 # Input float dos dados com try-except.
 try:
     # Recebe float da quantidade de diárias com o carro.
-    v_diaria = float(input("Digite o número de diárias: "))    
+    v_diaria = int(input("Digite o número de diárias: "))    
     # Se o valor da diária for menor que 1, abre except e fecha o programa.
     if v_diaria < 1:
         raise
