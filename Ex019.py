1f0a7c3c272808ac3ffac50454ab68038c2cb84d
Felipe Pereira <felipewnp@gmail.com>
2020-04-11 19:08:42 -0300

    Correcoes e comentarios Exercicio 19

--- Ex019.py
+++ Ex019.py
@@ -1,19 +1,30 @@
-from random import choice, randint
+from random import choice
+from sys import maxsize
+#from random import randint
 
+# Inicialização de variáveis.
 alunos = []
-nome = i = 0
+nome = ""
+i = 0
 
-#while i < 99999+1:
-while True:
-    nome = str(input("Digite o nome do aluno: "))
-    #nome = str(randint(-9223372036854775808,9223372036854775806))
-    alunos.append(nome)
-    i += 1 
-    if alunos[-1] == "end":
-        alunos.pop()
-        break
-    if alunos[-1] == "":
-        alunos.pop()
+# Inicializa maxlistsize como tamanho máximo de int no sistema
+maxlistsize = maxsize - 1
+
+# Loop, de tamanho maxlistsize, de inserção de dados.
+while i < maxlistsize+1:
+    nome = str(input("Digite o nome do aluno: ")) # Adiciona input como string em nome.
+    #nome = str(randint(-9223372036854775808,9223372036854775806)) # Teste.
+    alunos.append(nome) # Adiciona nome na lista alunos.
+    i += 1  # Incrementa +1 no contador i.
+    if alunos[-1] == "end": # Se último nome inserido em alunos == end:
+        alunos.pop() # Deleta último valor inserido.
+        break # Encerra o loop.
+    if alunos[-1] == "": # Se último nome inserido em alunos for blank:
+        alunos.pop() # Deleta último valor inserido.
+        i -= 1 # Decrementa -1 no contador i.
         print("Valor inválido")
 
-print(choice(alunos))
+if alunos:
+    print(choice(alunos)) # Escolhe um elemento da lista
+else:
+    print("Nenhum nome na lista!")
